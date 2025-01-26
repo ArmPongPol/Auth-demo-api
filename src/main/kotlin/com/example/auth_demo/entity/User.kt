67ca43e8ec488c17f15f1f6026fee8108726c3ba
armpongpol <pongpol095@gmail.com>
2025-01-26 12:28:09 +0700

    commit: docker container

--- src/main/kotlin/com/example/auth_demo/entity/User.kt
+++ src/main/kotlin/com/example/auth_demo/entity/User.kt
@@ -2,6 +2,8 @@ package com.example.auth_demo.entity
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
 import java.util.Date
 
 @Entity
@@ -13,8 +15,10 @@ data class User(
   val username: String,
   val password: String,
   val email: String,
-  @JsonFormat(pattern = "yyyy-MM-dd'h'HH:mm:ss")
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+  @CreationTimestamp
   val createdAt: Date = Date(),
-  @JsonFormat(pattern = "yyyy-MM-dd'h'HH:mm:ss")
+  @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+  @UpdateTimestamp
   val updatedAt: Date = Date()
 )
